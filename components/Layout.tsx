ae2a62c49c8cb9f6ffee192d68d916bc33d63ccb
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-06 01:42:31 +0530

    create: created home component

---
+++ components/Layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return <div className="w-full max-w-6xl h-full p-4">{children}</div>;
+};
+
+export default Layout;
